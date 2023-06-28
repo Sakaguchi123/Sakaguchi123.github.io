0bdf6b12d00073147690f7b5bcdd77e37fe2d685
Sakaguchi123 <133064214+Sakaguchi123@users.noreply.github.com>
2023-06-28 13:41:56 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,256 @@
+'use strict'
+
+
+//問題
+const meat = [{name: "ザブトン",price: 500,judg: "zabuton",imgs: "meat1"},
+              {name: "牛タン",price: 500,judg: "gyuutan",imgs: "Tan"},
+              {name: "ハラミ",price: 500,judg: "harami",imgs: "meat2"},
+              {name: "カルビ",price: 500,judg: "karubi",imgs: "meat3"},
+              {name: "牛ヒレ",price: 500,judg: "gyuuhire",imgs: "meat4"},
+              {name: "サーロイン",price: 500,judg: "sa-roin",imgs: "meat1"},
+              {name: "シャトーブリアン",price: 500,judg: "syato-burian",imgs: "meat6"},
+              {name: "ミスジ",price: 500,judg: "misuji",imgs: "meat2"},
+              {name: "牛肩ロース",price: 500,judg: "gyuukataro-su",imgs: "meat3"},
+              {name: "イチボ",price: 500,judg: "itibo",imgs: "meat4"},
+              {name: "サガリ",price: 500,judg: "sagari",imgs: "meat5"},
+              {name: "シマチョウ",price: 500,judg: "simatyou",imgs: "meat1"},
+              {name: "カイノミ",price: 500,judg: "kainomi",imgs: "meat2"},
+              {name: "トモサンカク",price: 500,judg: "tomosankaku",imgs: "meat3"},
+              {name: "ミノ",price: 500,judg: "mino",imgs: "meat4"},
+              {name: "牛ハツ",price: 500,judg: "gyuuhatu",imgs: "meat1"},
+              {name: "ランプ",price: 500,judg: "ranpu",imgs: "meat2"},
+              {name: "マルチョウ",price: 500,judg: "maruchou",imgs: "meat3"},
+              {name: "牛レバー",price: 500,judg: "gyuureba-",imgs: "meat4"},
+              {name: "リブロース",price: 500,judg: "riburo-su",imgs: "meat1"},
+              {name: "センマイ",price: 500,judg: "senmai",imgs: "meat2"},
+              {name: "牛カシラ",price: 500,judg: "gyuukasira",imgs: "meat3"},
+              {name: "牛フォアグラ",price: 500,judg: "gyuufoagura",imgs: "meat5"},
+              {name: "スネ",price: 500,judg: "sune",imgs: "meat1"},
+              {name: "テール",price: 500,judg: "te-ru",imgs: "meat2"},
+              {name: "ピーマン",price: 500,judg: "pi-man",imgs: "GreenPepper"},
+              {name: "玉ねぎ",price: 500,judg: "tamanegi",imgs: "Onion"},
+              {name: "焼きエビ",price: 500,judg: "yakiebi",imgs: "Shrimp"}
+]
+
+
+
+//html要素を取得する
+const title = document.querySelector(".title");
+const img1 = document.querySelector(".img1");
+const metaImg = document.querySelector(".meat-img");
+const type1 = document.querySelector(".type1");
+const type2 = document.querySelector(".type2");
+const beepSound = document.getElementById("beep-sound");
+const correctSound = document.getElementById("correct-sound");
+const grilleSound = document.getElementById("meat-sound");
+const vol = document.querySelector(".vol");
+const unVol = document.querySelector(".unVol");
+const timerText = document.querySelector(".timer");
+
+
+
+//配列・変数の用意
+let judgment = "";
+let judgmentArr = [];
+let judgmentSpan;
+let num = 0;
+let intervalStop;
+let meatSoundControl = true;
+
+//画像処理
+let img = [];
+for (const key in meat) {
+  img.push(`img/${meat[key]["imgs"]}.png`)
+}
+
+
+//問題を表示させる
+function questions(type) {
+
+  //初期化
+  judgment = "";
+  judgmentArr = [];
+  num = 0;
+  let random = Math.floor(Math.random() * meat.length); //問題数に応じてランダムな数字を出力する
+  type.textContent = meat[random]["name"];              //htmlに表示させる(日本語)
+  judgment = (meat[random]["judg"]);                    //judgmentにローマ字表記の文字を入れる
+
+  //問題に紐づけた画像を表示させる
+  metaImg.src=img[random];
+  switch (img[random]) {
+    case "img/meat1.png" :
+      metaImg.style.width = "8%";
+      metaImg.style.left = "47%";
+      metaImg.style.bottom = "48%";
+      metaImg.style.rotate = "30deg";
+    break;
+    case "img/Tan.png" :
+      metaImg.style.width = "13%";
+      metaImg.style.left = "44%";
+      metaImg.style.bottom = "52%";
+      metaImg.style.rotate = "-20deg";
+    break;
+    case "img/meat2.png" :
+      metaImg.style.width = "8%";
+      metaImg.style.left = "47%";
+      metaImg.style.bottom = "48%";
+      metaImg.style.rotate = "-20deg";
+    break;
+    case "img/meat3.png" :
+      metaImg.style.width = "8%";
+      metaImg.style.left = "47%";
+      metaImg.style.bottom = "48%";
+      metaImg.style.rotate = "80deg";
+    break;
+    case "img/meat4.png" :
+      metaImg.style.width = "8%";
+      metaImg.style.left = "47%";
+      metaImg.style.bottom = "50%";
+      metaImg.style.rotate = "30deg";
+    break;
+    case "img/meat5.png" :
+      metaImg.style.width = "17%";
+      metaImg.style.left = "42%";
+      metaImg.style.bottom = "55%";
+      metaImg.style.rotate = "-20deg";
+    break;
+    case "img/meat6.png" :
+      metaImg.style.width = "13%";
+      metaImg.style.left = "44%";
+      metaImg.style.bottom = "55%";
+      metaImg.style.rotate = "20deg";
+    break;
+    case "img/GreenPepper.png" :
+      metaImg.style.width = "9%";
+      metaImg.style.left = "45%";
+      metaImg.style.bottom = "48%";
+      metaImg.style.rotate = "20deg";
+    break;
+    case "img/Onion.png" :
+      metaImg.style.width = "12%";
+      metaImg.style.left = "44%";
+      metaImg.style.bottom = "50%";
+      metaImg.style.rotate = "20deg";
+    break;
+    case "img/Shrimp.png" :
+      metaImg.style.width = "21%";
+      metaImg.style.left = "39%";
+      metaImg.style.bottom = "55%";
+      metaImg.style.rotate = "30deg";
+    break;
+  }
+
+  //入力後の文字の色を変える処理
+  judgmentArr = judgment.split("");                     //ローマ字を1文字ずつの配列に入れる
+  for (const oneArr of judgmentArr) {                   //1文字ずつ処理する
+    judgmentSpan = document.createElement("span");      //spanタグを作る
+    judgmentSpan.innerText = oneArr;                    //作成したspanタグに1文字入れる
+    type2.appendChild(judgmentSpan);                    //htmlのtype2に作成したspanタグを追加する。
+    judgmentSpan.classList.add("default");              //classをdefaultに設定
+  }
+}
+
+//キーが押されたときに動作する関数
+document.addEventListener("keydown",keyDown);  //キーを押すごとにkeydownイベントが発生。2個めの引数に関数を指定した場合キーが押されたとき処理が実行される
+
+function keyDown(e) {                          //eのみだとkeyboardEventオブジェクトが出力　ドット記法でkeyというキーを選択して値を出力する。
+  //スタート処理
+  if (e.key === "Enter" && title.textContent === "食べる【Enter】") {   //Enterキーが押されたら
+    title.textContent = "START!!";                                      //titleをスタートに書き換える
+    
+    //1秒後にtitleを消す
+    let timeOut = function () {
+      title.textContent = "";                       //titleを空欄にする
+      questions(type1);                             //肉(問題)を表示
+      intervalStop = setInterval(timerStart,1000); //1秒ごとにtimerStart関数を実行する
+      meatSound();
+
+    };
+    setTimeout(timeOut, 1000); //timeOut呼び出し、秒数の指定
+  }
+  
+  //入力判定
+  let judgValue = type2.querySelectorAll("span");                         //type2の中のspanタグをjudgValueに入れる
+  if (e.key === judgValue[num].innerText && title.textContent === "") {   //入力したキーが合っているか判定
+    judgValue[num].classList.remove("default");                           //classを削除
+    judgValue[num].classList.remove("unCorrect");                         //classを削除
+    judgValue[num].classList.add("correct");                              //classを追加
+    num++;                                                                //次の文字へ行くため1プラスする
+  }else {                                                                 //入力した文字が合っていなかった場合
+    beep();
+    judgValue[num].classList.remove("default");                           //classを削除
+    judgValue[num].classList.add("unCorrect");                            //classを追加(文字が赤くなる)
+  }
+  
+  if (num === judgValue.length) {
+    //全文字入力し終わったら
+    correct();
+    for(let i = 0; i < judgValue.length; i++) {                           //type2の中のspanタグを全て削除する
+      const spanElement = type2.querySelector("span");                    //spanタグを一つ持ってくる
+      type2.removeChild(spanElement);                                     //spanタグを削除する
+    }
+    questions(type1);                                                     //次の問題をセットする
+  } else if (e.key === "Escape") {
+    gameEnd();
+  }
+}
+
+
+//終了処理
+function gameEnd() {
+  title.textContent = "食べる【Enter】";          //初期画面に戻す
+  type1.textContent = "";
+  type2.textContent = "";
+  metaImg.src = "";
+}
+
+
+//タイマー実行関数
+let count = 60;                                 //countを60秒に設定
+function timerStart() {
+  if(count > 0) {                               //countがゼロ以上であれば
+    count--;                                    //-1する
+    timerText.textContent = count;              //htmlにcountを表示する
+
+  } else if (count === 0) {                     //countが0になったら
+    clearInterval(intervalStop);                //setIntervalを停止させる
+
+    timerText.textContent = 60;                 //初期値を挿入
+    gameEnd();                                  //ゲームを終了させる
+  }
+}
+
+
+
+
+//効果音
+function beep() {
+  beepSound.currentTime = 0;
+  beepSound.play();
+}
+function correct() {
+  correctSound.currentTime = 0;
+  correctSound.play();
+}
+//ミュートボタン処理
+function mute() {
+  if (beepSound.muted) {
+    beepSound.muted = false;            //false = 音が出る
+    correctSound.muted = false;
+    unVol.style.visibility = "hidden";
+    vol.style.visibility = "visible";
+  }else {
+    beepSound.muted = true;             //true = 音が出ない
+    correctSound.muted = true;
+    unVol.style.visibility = "visible";
+    vol.style.visibility = "hidden";
+  }
+}
+
+//肉を焼く効果音
+function meatSound() {
+  grilleSound.currentTime = 0;
+  grilleSound.play();
+  meatSoundControl = true;
+  grilleSound.loop = meatSoundControl;
+}
